578a24f95d74626e3c29e02e2a16d1df41e730ed
MykolaRudnevjs <rudnevmykola@gmail.com>
2021-03-01 21:46:18 +0100

    creating persone data type

---
+++ schemas/person.js
@@ -0,0 +1,39 @@
+import { MdPerson as icon } from 'react-icons/md';
+
+export default {
+  name: 'persone',
+  title: 'Slicemasters',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      description: 'Tell us a bit about this persone',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
